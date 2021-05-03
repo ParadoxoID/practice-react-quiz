a923afa53a0b172736b8178df54edbbff70228a0
ParadoxoID <asker01adig@gmail.com>
2021-05-03 12:56:42 +0300

    Добавил боковое меню с анимацие. Для создания своих опросов в будущем пригодится.

---
+++ src/components/Navigation/Drawer/Drawer.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import classes from './Drawer.css';
+
+const links = [1, 2, 3];
+
+class Drawer extends React.Component {
+  renderLinks() {
+    return links.map((link, index) => {
+      return (
+        <li key={index}>
+          <a>LINK {link}</a>
+        </li>
+      );
+    });
+  }
+
+  render() {
+    const cls = [classes.Drawer];
+
+    if (!this.props.isOpen) {
+      cls.push(classes.close);
+    }
+    return (
+      <React.Fragment>
+        <nav className={cls.join(' ')}>
+          <ul>{this.renderLinks()}</ul>
+        </nav>
+        {this.props.isOpen ? <Backdrop onClick={this.props.onClose} /> : ''}
+      </React.Fragment>
+    );
+  }
+}
+
+export default Drawer;
