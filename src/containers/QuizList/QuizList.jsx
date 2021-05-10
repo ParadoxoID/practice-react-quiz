310542057fbbb26254114ff82e7c9c3b7ace3fc2
ParadoxoID <asker01adig@gmail.com>
2021-05-10 13:46:58 +0300

    Добавил компонент лоадера, который показыается пока не ответит сервер.

--- src/containers/QuizList/QuizList.jsx
+++ src/containers/QuizList/QuizList.jsx
@@ -1,16 +1,47 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import Loader from '../../components/UI/Loader/Loader';
 import { NavLink } from 'react-router-dom';
 import classes from './QuizList.css';
 
 export default class QuizList extends Component {
+  state = {
+    quizes: [],
+    loading: true
+  };
+
   renderQuizes() {
-    return [1, 2, 3].map((quiz, index) => {
+    return this.state.quizes.map(quiz => {
       return (
-        <li key={index}>
-          <NavLink to={'/quiz/' + quiz}>Test {quiz}</NavLink>
+        <li key={quiz.id}>
+          <NavLink to={'/quiz/' + quiz.id}>{quiz.name}</NavLink>
         </li>
       );
     });
+  }
+
+  async componentDidMount() {
+    try {
+      const response = await axios.get(
+        'https://react-quiz-v2-default-rtdb.europe-west1.firebasedatabase.app/quizes.json'
+      );
+
+      const quizes = [];
+
+      Object.keys(response.data).forEach((key, index) => {
+        quizes.push({
+          id: key,
+          name: `Тест №${index + 1}`
+        });
+      });
+
+      this.setState({
+        quizes,
+        loading: false
+      });
+    } catch (e) {
+      console.log(e);
+    }
   }
 
   render() {
@@ -19,7 +50,11 @@ export default class QuizList extends Component {
         <div>
           <h1>Список тестов</h1>
 
-          <ul>{this.renderQuizes()}</ul>
+          { this.state.loading 
+              ? <Loader /> 
+              : <ul>{this.renderQuizes()}</ul>
+          }
+
         </div>
       </div>
     );
